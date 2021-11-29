d08ca25902b2b23b8e5b513ab7c5804d2499ebd9
Aleksadr Tcibizov <tcibizov.ac@gmail.com>
2021-11-29 17:36:20 +0300

    rebase 2

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -1,9 +1,8 @@
+#!/bin/bash
 # display command line options
 
 count=1
-for param in "$@"; do
-    echo "Parameter: $param"
+for param in "$*"; do
+    echo "\$* Parameter #$count = $param"
     count=$(( $count + 1 ))
 done
-
-echo "====="
