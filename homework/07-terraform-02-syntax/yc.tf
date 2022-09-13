1ec7089578cbb957606b9df528dcbe67bfa90bfa
Aleksandr Tsibizov <92942483+Tcibizov@users.noreply.github.com>
2022-09-13 16:10:56 +0400

    Update yc.tf

--- homework/07-terraform-02-syntax/yc.tf
+++ homework/07-terraform-02-syntax/yc.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "yandex" {
-  token     = "AQAAAAA-eODYAATuwQfQsl6BcUb1rMFT7Bqmw0Y"
+  token     = "...........FT7Bqmw0Y"
   cloud_id  = "b1gsin4cial4tfskfcu9"
   folder_id = "b1gbmv9vnj1ko7uqevp7"
   zone      = "ru-central1-b"
